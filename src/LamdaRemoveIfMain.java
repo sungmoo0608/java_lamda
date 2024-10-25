99b9e2e6b8aae254c47ed02ea3c7d8a15ffad3e4
WIN-3QL0R8Q3DTD\ICT02_03 <swingsm0608@gmail.com>
2024-10-25 09:58:43 +0900

    removeif 연습

---
+++ src/LamdaRemoveIfMain.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class LamdaRemoveIfMain {
+
+	public static void main(String[] args) {
+		
+		List<Integer> ls = Arrays.asList(1,-2,3,4,5);
+		
+		ls = new ArrayList<>(ls);
+		
+		Predicate<Number> p = n -> n.doubleValue() < 0.0; //삭제 조건
+		
+		//boolean test(T t);
+		Predicate<Integer> p1 = (Integer i) -> {
+			return ( i < 0 ) ? true:false; 
+		};
+		
+		ls.removeIf(p1);
+		System.out.println(ls);
+		
+		ls.removeIf(p);
+		System.out.println(ls);
+		
+	}
+
+}
